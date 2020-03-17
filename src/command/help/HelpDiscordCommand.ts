7efa58079642acb272da1b1ec1c829b0b4a18db4
COVID-GUH <contact@covid19.fyi>
2020-03-17 16:43:54 -0400

    test run

--- src/command/help/HelpDiscordCommand.ts
+++ src/command/help/HelpDiscordCommand.ts
@@ -1,6 +1,6 @@
 import { DiscordCommand } from "../DiscordCommand";
 
-export class InvalidDiscordCommand extends DiscordCommand {
+export class HelpDiscordCommand extends DiscordCommand {
   public async execute(): Promise<void> {
     await this.message.channel.send("Help Menu");
   }
